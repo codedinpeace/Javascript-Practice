f733892a510c958d1544b9311dd9bf066d6ca423
codedinpeace <vea5a.pranam05@gmail.com>
2026-02-18 11:01:16 +0530

    PromiseQueues

---
+++ Day3/Hard/PromiseQueues.js
@@ -0,0 +1,13 @@
+let arr = [async = ()=>{
+    console.log("Hi")
+}, async = () =>{
+    console.log("Hello")
+}, async = () =>{
+    console.log("Really Good")
+}, async = () =>{
+    console.log("Guten Morgen! Ich bin Pranam, ich bin student. Ich komme aus Indien, Ich lobe essen und malst")
+} ]
+
+arr.map((func)=>{
+    func()
+})
